646c2528ec1d4e05bc71015a6449c3146136de54
uu <649673002@qq.com>
2016-12-28 10:00:32 +0800

    senior entrance-eaxm 12-28

---
+++ golden_manage/webs/src/pages/AjaxData/Exam/EntranceAnalysis.js
@@ -0,0 +1,690 @@
+
+//年级，班级考试统计信息
+export var gradeExamMap = {
+	status: 'success',
+
+	result: {
+		gradeExamMap: {
+			grades: [
+				{
+					gradeName: '初一',
+					gradeID: '2016'
+				},
+			],
+
+			classMap: {
+				2016: [
+					{
+						classID: '1',
+						className: '初一1班'
+					}
+				]
+			},
+
+			examMap: {
+				2016: [
+					{
+						examID: '1',
+						examName: '2016期中考试',
+					}
+				]
+			}
+		}
+	}
+
+};
+
+
+//新中考规则各班排名变动：(包含了预选科总分)
+export var classRankInfos = {
+	status: 'success',
+	result: {
+		classRankInfos: [
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				className: '初二1班',
+				classID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			}
+			
+		] 
+	}
+}
+
+
+export var studentsRankInfos = {
+	status: 'success',
+	result: {
+		studentsRankInfos: [
+			{
+				studentName: '赵一一',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				studentName: '赵',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				studentName: '赵',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			},
+			{
+				studentName: '钱',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: -2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '孙',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:3
+				}
+			},
+			{
+				studentName: '李',
+				studentID: '1',
+				totalScore: {
+					rank: 3,
+					score: 722
+				},
+				newEntranceScore: {
+					rank: 1,
+					score: 486,
+					changeRank: 2,
+				},
+
+				preChooseSubjsScore: {
+					rank: 2,
+					score: 468,
+					changeRank:-3
+				}
+			}
+			
+		] 
+	}
+}
+
+
+//开启预选科功能：
+export var startPreChooseSubjs = {
+	status: 'success',
+	code: 0  // 0, 条件满足， 1： 没有使用选科模块， 2： 没有收集完全预选科结果
+}
--- golden_manage/webs/src/pages/Exam/SeniorEntranceExamRuleChooseSubject/GradeAnalysis/GradeAnalysis.js
+++ golden_manage/webs/src/pages/Exam/SeniorEntranceExamRuleChooseSubject/GradeAnalysis/GradeAnalysis.js
@@ -1,17 +1,75 @@
 import React from 'react'
+import RankChangeTable from '../components/RankChangeTable'
+import { gradeExamMap,classRankInfos } from '../../../AjaxData/Exam/EntranceAnalysis'
+import ExamFilter from '../components/ExamFilter' 
+import * as SeniorExtranceUtil from '../SeniorEntranceExamUtil'
+
+const handleRankData = SeniorExtranceUtil.handleRankData
+const handleSelectChange = SeniorExtranceUtil.handleSelectChange
 
 class GradeAnalysis extends React.Component
 {
 	constructor( props )
 	{
 		super( props );
+
+		this.type = 'grade';
+	}
+
+	componentWillMount()
+	{
+		this.queryData();
+	}
+
+	componentWillReceiveProps( newProps )
+	{
+		this.queryData();
+	}
+
+	queryData()
+	{//请求相关数据
+		var success = function( data )
+		{
+			if( data.status === 'success' && data.result )
+			{
+				if(data.result.classRankInfos)
+				{
+					var classRankInfos = data.result.classRankInfos&&data.result.classRankInfos;
+					var classRankData = handleRankData(classRankInfos);
+					this.setState({classRankData});
+				}
+				else
+				{
+					var gradeExamData = data.result.gradeExamMap&&data.result.gradeExamMap;
+					this.setState({gradeExamData});
+				}			
+			}
+		},
+		fail = function()
+		{
+		}
+
+		success.call(this,classRankInfos);
+		success.call(this,gradeExamMap);
+
+		this.setState({
+			computeWay:'standardScore'
+		})
 	}
 
 	render()
 	{
-		return <div>
-			GradeAnalysis
-		</div>
+		var t = this;
+		var type = t.type;
+		var { classRankData,gradeExamData,computeWay } = t.state;
+		var ruleTypeMap = SeniorExtranceUtil.ruleTypeMapGrade;
+		
+		return (
+			<div>
+				<ExamFilter mapData={gradeExamData} type={type} handleSelectChange={handleSelectChange.bind(t)} computeWay={computeWay}/>
+				<RankChangeTable rankData={classRankData} ruleTypeMap={ruleTypeMap} type={type} />
+			</div>
+		)
 	}
 }
 
